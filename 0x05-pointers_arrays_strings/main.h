22ba22c91dcdcca33a632198fbc78f26d17d2c47
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-03-27 22:07:22 +0200

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,18 +1,4 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+void print_to_98(int n);
+#endif
