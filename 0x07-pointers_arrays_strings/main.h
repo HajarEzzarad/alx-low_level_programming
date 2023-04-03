2114f4302f56020445a66f7a29ce2622350d16ee
hajar ezzarad <hajarezzarad2002@gmail.com>
2023-04-03 10:24:59 -0700

    update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
 /*
  * file : main.h
  * its a heder file containing protypes for all my functiond
  */
 
-int _putchar(char c)
-{
-	return(write(1,&c,1));
-}
 
 void *_memset(void *s,char b,unsigned int a);
 #endif
