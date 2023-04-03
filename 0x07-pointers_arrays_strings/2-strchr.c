627e029d5b959257fc435b32771ba96158f21f83
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-03 22:34:48 +0200

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,16 +5,14 @@
  * @c: character.
  * Return: the pointer to the first occurrence of the character c.
  */
- char *_strchr(char *s, char c)
- {
-   unsigned int i;
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
 
- 	for (i=0; *(s + i) != '\0'; i++)
- {
+	for (i=0; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
 	if (*(s + i) == c)
 		return (s + i);
-  }
- 	if (*(s + i) == c)
-		return (s + i);
- 	return ('\0');
- }
+	return ('\0');
+}
