e667b1e2988a49f0831e0a1d5d63a52683508f86
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-04 02:20:52 +0200

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,16 +7,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, m = 0;
+	int i;
+	int j;
 
-	for (i = 0; i < 64; i++)
+	for (i = 0; i < 8; i++)
 	{
-		if (i % 8 == 0 && i != 0)
+		for (j = 0; j < 8; j++)
 		{
-			m = i;
-			_putchar('\n');
+			_putchar(a[i][j]);
 		}
-		_putchar(a[i / 8][i - m]);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
