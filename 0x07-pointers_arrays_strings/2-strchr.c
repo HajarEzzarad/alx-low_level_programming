da383e73a02e0d3efab7aaa8c558a0674fe3632c
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-04 02:23:06 +0200

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i;
 
-	for (i=0; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
+	for (i = 0; *(s+i) != '\0'; i++)
+	{
+		if (*(s+i) == c)
+		{
 			return (s + i);
-	if (*(s + i) == c)
+		}
+	}
+	if (*(s+i) == c)
 		return (s + i);
-	return ('\0');
+	return (0);
 }
