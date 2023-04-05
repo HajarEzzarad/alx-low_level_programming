dab3f7ba2a0f35c3fbfe95318aa198fb0ea42357
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-05 22:30:49 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0') 
-{ 
-_putchar('\n');
-return;
-} 
-else
-{
-_putchar(*s);
-_puts_recursion(s + 1); 
-}
+	if (*s == '\0')
+	{
+    _putchar('\n');
+	}
+	else
+		_putchar(*s);
+		_puts_recursion(s + 1);
 }
