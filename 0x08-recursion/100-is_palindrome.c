5a9aa78c17004fa53f897e6c200636f2d12025da
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-06 00:09:48 +0200

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,9 +9,13 @@
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
-		return (0);
+{
+return (0);
+}
 else
-		return (1 + _strlen_recursion(s + 1));
+{
+return (1 + _strlen_recursion(s + 1));
+}
 }
 
 /**
@@ -29,7 +33,7 @@ return (1);
 }
 if (s[start] != s[end])
 {
-return 0;
+return (0);
 }
 return (helper(s, start + 1, end - 1));
 }
@@ -41,6 +45,8 @@ return (helper(s, start + 1, end - 1));
 int is_palindrome(char *s)
 {
 if (*s == '\0')
-		return (1);
+{
+return (1);
+}
 return (helper(s, 0, _strlen_recursion(s) - 1));
 }
