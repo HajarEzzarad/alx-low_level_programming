ca070276a394f27dec977c7baf16188fe5d533c3
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-06 00:32:47 +0200

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,15 +18,15 @@ if (*(s2+1) == '\0')
 {
 return (1);
 }
-if (*s1 != '\0' && wildcmp(s1+1, s2))
+if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 {
 return (1);
 }
-return (wildcmp(s1, s2+1));
+return (wildcmp(s1, s2 + 1));
 }
 else if (*s1 == *s2)
 {
-return (wildcmp(s1+1, s2+1));
+return (wildcmp(s1 + 1, s2 + 1));
 }
 else
 {
