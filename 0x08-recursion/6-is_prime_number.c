955b7b9b0b6d27ca647bf3bfc1202eb6c5980a1a
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-05 23:50:03 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,25 +1,34 @@
 #include "main.h"
 
 /**
- * is_prime - detects if an input number is a prime number.
- * @n: input number.
- * @c: iterator.
+ * _prime_helper - detects if an input number is a prime number.
+ * @n: check for primality.
+ * @i: for the loop that checks for divisibility.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-int is_prime_helper(int n, int i) {
-    if (n <= 2) {
-        return (n == 2) ? 1 : 0;
-    }
-    if (n % i == 0) {
-        return 0;
-    }
-    if (i * i > n) {
-        return 1;
-    }
-    return is_prime_helper(n, i + 1);
+int _prime_helper(int n, int i)
+{
+if (n <= 2)
+{
+return ((n == 2) ? 1 : 0);
 }
-
-int is_prime_number(int n) {
-    return is_prime_helper(n, 2);
+if (n % i == 0)
+{
+return (0);
+}
+if (i * i > n)
+{
+return (1);
+}
+return _prime_helper(n, i + 1);
+}
+/**
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
+int is_prime_number(int n)
+{
+return (_prime_helper(n, 2));
 }
 
