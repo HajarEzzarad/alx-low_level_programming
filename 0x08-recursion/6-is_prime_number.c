80fe4f196646d1691b5efcd777d8db6d54e55ce4
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-05 23:44:35 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,30 +6,20 @@
  * @c: iterator.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-int is_prime_number(int n)
-{
-int i;
-if (n <= 1) 
-{
-return 0;
+int is_prime_helper(int n, int i) {
+    if (n <= 2) {
+        return (n == 2) ? 1 : 0;
+    }
+    if (n % i == 0) {
+        return 0;
+    }
+    if (i * i > n) {
+        return 1;
+    }
+    return is_prime_helper(n, i + 1);
 }
-else if (n == 2 || n == 3)
-{
-return 1;
+
+int is_prime_number(int n) {
+    return is_prime_helper(n, 2);
 }
-else if (n % 2 == 0 || n % 3 == 0)
-{
-return 0;
-}
-else
-{
-for (i = 5; i*i <= n; i += 6)
-{
-if (n % i == 0 || n % (i+2) == 0)
-{
-return 0;
-}
-}
-return 1;
-}
-}
+
