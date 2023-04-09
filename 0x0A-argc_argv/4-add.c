fe5706616e9b6ac33ad6e53977a9b1691d6657b0
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-09 22:18:02 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,24 +7,22 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
 
-int main(int argc, char *argv[]) {
-    int sum = 0;
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
-    for (int i = 1; i < argc; i++) {
-        char *str = argv[i];
-        while (*str) {
-            if (!isdigit(*str)) {
-                printf("Error\n");
-                return 1;
-            }
-            str++;
-        }
-        sum += atoi(argv[i]);
-    }
-    printf("%d\n", sum);
-    return 0;
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
 }
