d049c8b0a64e6c95a35bc85a23bdce01a2df5235
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-11 16:05:29 +0200

    create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,25 +8,20 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char *arr;
-    
-    if (size == 0)
-    {
-        return (NULL);
-    }
-    
-    arr = malloc(size * sizeof(char));
-    
-    if (arr == NULL)
-    {
-        return (NULL);
-    }
-    
-    for (i = 0; i < size; i++)
-    {
-        arr[i] = c;
-    }
-    
-    return (arr);
+unsigned int i;
+char *arr;
+if (size == 0)
+{
+return (NULL);
+} 
+arr = malloc(size * sizeof(char)); 
+if (arr == NULL)
+{
+return (NULL);
+} 
+for (i = 0; i < size; i++)
+{
+arr[i] = c;
+} 
+return (arr);
 }
