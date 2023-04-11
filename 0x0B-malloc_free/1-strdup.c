cf85dae066bad039cd701b5110981ebc660599fe
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-11 19:59:17 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,22 +9,12 @@
  */
 char *_strdup(char *str)
 {
-	char *strout;
-	unsigned int i, j;
-
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-
-	strout = (char *)malloc(sizeof(char) * (i + 1));
-
-	if (strout == NULL)
-		return (NULL);
-
-	for (j = 0; j <= i; j++)
-		strout[j] = str[j];
-
-	return (strout);
+size_t len = strlen(str) + 1;
+char *dup_str = malloc(len);
+if (dup_str == NULL)
+{
+return (NULL);
+}
+memcpy(dup_str, str, len);
+return (dup_str);
 }
