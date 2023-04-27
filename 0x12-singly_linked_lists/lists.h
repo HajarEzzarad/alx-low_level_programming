44624b93cc18a7739e5663a0f73a3cd608ca7459
HajarEzz <107047829+HajarEzzarad@users.noreply.github.com>
2023-04-27 23:07:21 +0200

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
-#include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
+int _putchar(char c);
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
