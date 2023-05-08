2889174d75cbf2100666d7f5539973e41ca82e3a
hajar ezzarad <hajarezzarad2002@gmail.com>
2023-05-08 13:46:59 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(file, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(file);
+
+	return (1);
+}
